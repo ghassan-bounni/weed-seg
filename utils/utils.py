655a63a1f3575f4488f2870f100eaad32bd11dc4
ghassan-bounni <ghassan.bounni18@gmail.com>
2023-10-20 17:48:54 +0300

    Remove unnecessary args from parser

--- utils/utils.py
+++ utils/utils.py
@@ -29,14 +29,8 @@ def parse_args():
     parser.add_argument(
         "--output", default="output", help="Path to the output directory."
     )
-    parser.add_argument("--log", default="logs.txt", help="Path to the log file.")
-    parser.add_argument(
-        "--device", default="cpu", help="Device to run the pipeline on."
-    )
     parser.add_argument("--seed", default=42, help="Seed for reproducibility.")
-    parser.add_argument(
-        "--verbose", default=False, help="Whether to print the logs or not."
-    )
+
     args = parser.parse_args()
     return args
 
